35703189bdd6ab15846927373fd8c4cca34b4b57
erickahr86 <erickahr86@gmail.com>
2021-10-31 00:52:02 -0500

    Second Commit

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/common/Resource.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/common/Resource.kt
@@ -1,8 +1,7 @@
 package com.plcoding.cryptocurrencyappyt.common
 
-sealed class Resource<T> ( val data: T? = null, val message: String? = null )
-{
-    class Success<T>( data: T ): Resource<T>( data )
-    class Error<T>( message: String, data: T? = null ): Resource<T>( data, message )
-    class Loading<T>( data: T?  = null ): Resource<T>( data )
+sealed class Resource {
+    class Success<T>( var data: T ): Resource()
+    class Error( var message: String): Resource()
+    class Loading: Resource()
 }
